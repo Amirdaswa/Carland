160da4995065fa22e8e08aced46c5890e899283b
Amirali Alizadeh <amir.ali.alizade.2009@gmail.com>
2023-11-02 10:20:04 +0330

    the navbar is almost ready but still it needs some transition and some  bases of cars card

---
+++ src/components/cars.tsx
@@ -0,0 +1,66 @@
+
+const Cars = () => {
+  return (
+    <div className="cars ml-8 mt-10 grid grid-cols-3 ">
+      <div className="car w-80 h-96 bg-gray-200 rounded-xl overflow-hidden  mt-24
+      ">
+          <header className="car__header rounded-xl"><img className="car__image w-full  " src="./src/images/peakpx.jpg" alt="" /></header>
+          <div className="car__body text-center">
+              <h1 className="car__name mt-4 text-lg font-bold">Maybach 2020</h1>
+              <h3 className="car__brand text-sm font-light"> Mercedes Benz</h3>
+              <h2 className="car__price mt-2">200/000$</h2>
+          </div>
+          
+      </div>
+      <div className="car w-80 h-96 bg-gray-200 rounded-xl overflow-hidden mt-24
+      ">
+          <header className="car__header rounded-xl"><img className="car__image w-full  " src="./src/images/peakpx.jpg" alt="" /></header>
+          <div className="car__body text-center">
+              <h1 className="car__name mt-4 text-lg font-bold">Maybach 2020</h1>
+              <h3 className="car__brand text-sm font-light"> Mercedes Benz</h3>
+              <h2 className="car__price mt-2">200/000$</h2>
+          </div>
+          
+      </div>
+      <div className="car w-80 h-96 bg-gray-200 rounded-xl overflow-hidden  mt-24
+      ">
+          <header className="car__header rounded-xl"><img className="car__image w-full  " src="./src/images/peakpx.jpg" alt="" /></header>
+          <div className="car__body text-center">
+              <h1 className="car__name mt-4 text-lg font-bold">Maybach 2020</h1>
+              <h3 className="car__brand text-sm font-light"> Mercedes Benz</h3>
+              <h2 className="car__price mt-2">200/000$</h2>
+          </div>
+          
+      </div>
+      <div className="car w-80 h-96 bg-gray-200 rounded-xl overflow-hidden mt-24">
+          <header className="car__header rounded-xl"><img className="car__image w-full  " src="./src/images/peakpx.jpg" alt="" /></header>
+          <div className="car__body text-center">
+              <h1 className="car__name mt-4 text-lg font-bold">Maybach 2020</h1>
+              <h3 className="car__brand text-sm font-light"> Mercedes Benz</h3>
+              <h2 className="car__price mt-2">200/000$</h2>
+          </div>
+          
+      </div>
+      <div className="car w-80 h-96 bg-gray-200 rounded-xl overflow-hidden mt-24">
+          <header className="car__header rounded-xl"><img className="car__image w-full  " src="./src/images/peakpx.jpg" alt="" /></header>
+          <div className="car__body text-center">
+              <h1 className="car__name mt-4 text-lg font-bold">Maybach 2020</h1>
+              <h3 className="car__brand text-sm font-light"> Mercedes Benz</h3>
+              <h2 className="car__price mt-2">200/000$</h2>
+          </div>
+          
+      </div>
+      <div className="car w-80 h-96 bg-gray-200 rounded-xl overflow-hidden mt-24 ">
+          <header className="car__header rounded-xl"><img className="car__image w-full  " src="./src/images/peakpx.jpg" alt="" /></header>
+          <div className="car__body text-center">
+              <h1 className="car__name mt-4 text-lg font-bold">Maybach 2020</h1>
+              <h3 className="car__brand text-sm font-light"> Mercedes Benz</h3>
+              <h2 className="car__price mt-2">200/000$</h2>
+          </div>
+          
+      </div>
+    </div>
+  )
+}
+
+export default Cars
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,46 +3,72 @@ import { useState } from "react"
 
 const Navbar = () => {
   const [sb,setSb]= useState(false);
+  
+
   const closeSB = (sb:boolean) =>{
     if(!sb){
       return (
-        <img onClick={()=>{setSb(true),console.log(sb)}} className="search__icon w-8 h-8 hover:cursor-pointer" src="./src/images/icons8-search-50.png" alt="" />
+        <img onClick={()=>{setSb(true),console.log(sb)}} className="search__icon w-8 h-8 hover:cursor-pointer ml-auto mr-auto " src="./src/images/icons8-search-50.png" alt="" />
       )
     }
     else{
       return (
 
-        <img onClick={()=>setSb(false)} className="close__icon mr-px w-8 h-8 hover:cursor-pointer" src="./src/images/icons8-close-50.png"  alt="" />
+        <img onClick={()=>{setSb(false),console.log(sb)}} className="close__icon mr-px w-8 h-8 hover:cursor-pointer" src="./src/images/icons8-close-50.png"  alt="" />
       )
     }
   }
   const SB = (sb:boolean)=>{
-    if(sb){
+    if(sb===true){
       return (
-        <input type="text" placeholder="Search For Cars" className="sb focus:outline-none mr-2 rounded border border-solid border-slate-200 p-2 w-10/12" />
+        
+          <nav className="sb__contaier flex w-full justify-center fixed  ">
+            <div className="sb flex w-3/5 m-auto border border-solid border-slate-300  rounded-md ">
+              <input type="text" placeholder="Search For Cars" className="sb__input  focus:outline-none
+              rounded-md border border-solid border-slate-200 rounded-r-none p-2 w-11/12 bg-slate-100 " />
+               <button className="sb__button bg-red-400 w-1/12 rounded-md flex justify-center items-center"><img className="search__icon w-7 h-7" src="./src/images/icons8-search-50.png" alt="" /></button>
+            </div>
+          </nav>
+      
         )
       }
-    else{
-      return(
-        <h1></h1>
+      else{
+        return(
+        <input type="text" placeholder="Search For Cars" className="sb  focus:outline-none mr-2 invisible
+         rounded border transition-opacity duration-500 border-solid border-slate-200 p-2 w-10/12" />
+
       )
     }
   }
   return (
-    <nav className="navbar items-center flex justify-between p-3 w-full bg-slate-50 shadow h-16">
-        <div className="navbar__logo w-14 h-14">
-          <img className="w-full h-full" src="./src/images/carland-low-resolution-logo-color-on-transparent-background.png" alt="" />
-        </div>
-        <div className="navbar__content flex w-2/4 justify-end">
+    
+    <>
+    <nav className="navbar items-center flex justify-between p-3 w-screen bg-slate-50 shadow h-16 ">
+        <div className="navbar__content w-2/4 flex items-center ">
           
-        {SB(sb)}
-            <ul className="navbar__items__list flex items-center gap-12">
+          <div className="navbar__items   ml-7px mr-10">
+              <ul className="navbar__items__list w-3/5 mr-10 flex ">
+                <li className="navbar__item mr-10 hover:text-red-600 whitespace-nowrap"><a href="#" className="items__link">Sell Car</a></li>
+                <li className="navbar__item mr-10 hover:text-red-600 whitespace-nowrap"><a href="#" className="items__link">Cars</a></li>
+                <li className="navbar__item mr-10 hover:text-red-600 whitespace-nowrap"><a href="#" className="items__link">Sign Up</a></li>
+              </ul>
+          </div>
+        </div>
+
+        <div className="navbar__sb flex w-2/4 justify-end w-9/12">
+          
+            <ul className="navbar__items__list flex items-center">
               {closeSB(sb)}
-              <img className="menu__icon mr-px w-8 h-8 hover:cursor-pointer" src="./src/images/icons8-menu-50.png"  alt="" />
             </ul>
         </div>
-    </nav>
+    </nav>    
+    <div className="sb">
+      {SB(sb)}
+    </div>
+    
+    </>
+
   )
 }
 
-export default Navbar 
+export default Navbar;
