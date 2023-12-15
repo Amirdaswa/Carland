91ba0923bec16283681853f2748d33edf5b70d3f
Amirali Alizadeh <amir.ali.alizade.2009@gmail.com>
2023-12-15 16:18:28 +0330

    The Car's viewing page is going to be ready

--- src/components/Carview.tsx
+++ src/components/Carview.tsx
@@ -2,15 +2,21 @@
 const Carview = () => {
   return (
     
-      <div className="Carview grid-cols-2 grid-rows-2 ">
-
-        <div className="carview__image ">
-          <img src="./src/images/peakpx.jpg" alt="" />
-        </div>
-        <div className="carview__informations">
-
+      <div className="h-full">
+        <div className="carview  " >
+          <div className="carview__card grid grid-cols-2 mt-16 mr-8 ml-8 bg-gray-200 h-192 ">
+            <div className=""><img src="./src/images/peakpx.jpg" alt="" className="carview__image rounded h-full w-full  " /></div>
+            <div className="carview__information">
+              <header className="carview__title">
+                <h1 className="carview__name">Maybach 2020</h1>
+                <h3 className="carview__brand"> MERCEDES BENZ</h3>
+              </header>
+            </div>
+          </div>
         </div>
       </div>
+     
+
 
   )
 }
